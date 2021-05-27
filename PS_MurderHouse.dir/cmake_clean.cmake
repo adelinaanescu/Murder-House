6277477e9958634f5f1efe67d0e03765f29adbda
adelinaanescu <84186552+adelinaanescu@users.noreply.github.com>
2021-05-27 15:05:33 +0300

    Add files via upload

---
+++ PS_MurderHouse.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PS_MurderHouse.dir/Game.cpp.obj"
+  "CMakeFiles/PS_MurderHouse.dir/main.cpp.obj"
+  "PS_MurderHouse.exe"
+  "PS_MurderHouse.exe.manifest"
+  "PS_MurderHouse.pdb"
+  "libPS_MurderHouse.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PS_MurderHouse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
